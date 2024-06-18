532c2093b2ee02798b43a1012e01dede6ce84588
Tom Pacino <tompacino@users.noreply.github.com>
2024-06-18 19:14:04 +1000

    Working on init script for postgres

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,5 +1,32 @@
+CREATE TABLE IF NOT EXISTS space (
+    id SERIAL PRIMARY KEY
+);
 
-create table Visits(
-    id      serial primary key,
-    time    timestamp
+CREATE TABLE IF NOT EXISTS hotdesk (
+    id         SERIAL PRIMARY KEY,
+    floor      INTEGER NOT NULL,
+    deskNumber INTEGER NOT NULL,
+    CONSTRAINT fk_space_id
+      FOREIGN KEY(id)
+        REFERENCES space(id)
+);
+
+CREATE TABLE IF NOT EXISTS room (
+    id         SERIAL PRIMARY KEY,
+    capacity   INTEGER NOT NULL,
+    roomNumber INTEGER NOT NULL,
+    usage      INTEGER NOT NULL,
+    CONSTRAINT fk_space_id
+      FOREIGN KEY(id)
+        REFERENCES space(id)
+);
+
+CREATE TABLE IF NOT EXISTS booking (
+    id         SERIAL PRIMARY KEY,
+    startTime  TIMESTAMP NOT NULL,
+    endTime    TIMESTAMP NOT NULL,
+    space_id   INTEGER NOT NULL,
+    CONSTRAINT fk_space_id
+      FOREIGN KEY(space_id)
+        REFERENCES space(id)
 );
