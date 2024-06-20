e583e7280526c8a57131315a7a262fb3e5af1099
Sofia De Bellis <sofia.debellis23@gmail.com>
2024-06-20 23:32:17 +1000

    WIP

--- frontend/app/(pages)/rooms/page.tsx
+++ frontend/app/(pages)/rooms/page.tsx
@@ -1,6 +1,79 @@
 import RoomCard from "@/app/components/roomCard";
+import Input from "@mui/joy/Input";
+import SearchIcon from "@mui/icons-material/Search";
+import FormControl from '@mui/joy/FormControl';
+import FormLabel from '@mui/joy/FormLabel';
+import * as React from 'react';
+import Select from '@mui/joy/Select';
+import Option from '@mui/joy/Option';
+import Box from '@mui/joy/Box';
+
+
+const renderFilters = () => (
+    <React.Fragment>
+      <FormControl size="sm">
+        <FormLabel>Type</FormLabel>
+        <Select
+          size="sm"
+          placeholder="Filter by room type"
+          slotProps={{ button: { sx: { whiteSpace: 'nowrap' } } }}
+        >
+          <Option value="meeting-room">Meeting Room</Option>
+          <Option value="consult-room">Consult Room</Option>
+          <Option value="conference-room">Conference Room</Option>
+        </Select>
+      </FormControl>
+      <FormControl size="sm">
+        <FormLabel>Capacity</FormLabel>
+        <Select size="sm" placeholder="All">
+          <Option value="all">All</Option>
+          <Option value="refund">1+</Option>
+          <Option value="purchase">2+</Option>
+          <Option value="debit">3+</Option>
+          <Option value="debit">4+</Option>
+          <Option value="debit">5+</Option>
+          <Option value="debit">6+</Option>
+        </Select>
+      </FormControl>
+      <FormControl size="sm">
+        <FormLabel>Customer</FormLabel>
+        <Select size="sm" placeholder="All">
+          <Option value="all">All</Option>
+          <Option value="olivia">Olivia Rhye</Option>
+          <Option value="steve">Steve Hampton</Option>
+          <Option value="ciaran">Ciaran Murray</Option>
+          <Option value="marina">Marina Macdonald</Option>
+          <Option value="charles">Charles Fulton</Option>
+          <Option value="jay">Jay Hoper</Option>
+        </Select>
+      </FormControl>
+    </React.Fragment>
+  );
+
 export default function Rooms() {
-    return (
-        <RoomCard />
-    )
+  return (
+    <>
+      <Box
+        className="SearchAndFilters-tabletUp"
+        sx={{
+          borderRadius: 'sm',
+          py: 2,
+          display: { xs: 'none', sm: 'flex' },
+          flexWrap: 'wrap',
+          gap: 1.5,
+          '& > *': {
+            minWidth: { xs: '120px', md: '160px' },
+          },
+        }}
+      >
+        <FormControl sx={{ flex: 1 }} size="sm">
+          <FormLabel>Search for a room</FormLabel>
+          <Input size="sm" placeholder="Search" startDecorator={<SearchIcon />} />
+        </FormControl>
+        {renderFilters()}
+      </Box>
+
+      <RoomCard />
+    </>
+  );
 }
