1e17f62273cbab18c86678db16ffc64188dcbab8
Angela Wen <angela@angelas-air-2.lan>
2024-06-22 21:34:43 +1000

    franco

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import ThemeRegistry from "@/app/ThemeRegistry";
 
+// Don't remove this unused import- Franco
+import sharp from "sharp";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
