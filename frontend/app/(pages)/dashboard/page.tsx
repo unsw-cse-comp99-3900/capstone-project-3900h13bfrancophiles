a52d5e5595e0b77014b8805683daec205c92a2fa
Angela Wen <angela@angelas-air-2.lan>
2024-06-22 21:21:00 +1000

    component

--- frontend/app/(pages)/dashboard/page.tsx
+++ frontend/app/(pages)/dashboard/page.tsx
@@ -1,36 +1,19 @@
-import {Box, Button, Card, CardContent, Stack, Typography} from "@mui/joy";
+import {Stack, Typography} from "@mui/joy";
+import CurrentBookingCard from "@/app/components/CurrentBookingCard";
 
 export default function Dashboard() {
-  return (
-    <Stack>
+  return (<Stack>
       <Typography level="h1">
         My Dashboard
       </Typography>
       <Typography level="h2" mb={2}>
         Current Booking
       </Typography>
-      <Card variant='outlined'>
-        <CardContent>
-          <Stack direction={{xs: 'column', lg: 'row'}} px={2} py={1} width='100%' justifyContent='space-between' alignItems={{ xs: 'flex-start', lg: 'center' }}>
-            <Box>
-              <Typography level="h2" sx={{ textWrap: 'wrap' }}>
-                K17 G02 - Consultation Room
-              </Typography>
-              <Typography level='body-lg' pb={1} sx={{ textWrap: 'wrap' }}>
-                Booked until 7:30 PM
-              </Typography>
-            </Box>
-            <Stack direction='row' spacing={{xs: 1, sm: 4}} height={60} width={{xs: '100%', lg: '50%'}} py='10px'  justifyContent='flex-end'>
-              <Button size="md" color="success" sx={{borderRadius: '20px', width: '130px', fontSize: {xs: '11pt', sm: '13pt'}}}>
-                Check in
-              </Button>
-              <Button size="md" color="danger" sx={{borderRadius: '20px', width: '180px', fontSize: {xs: '11pt', sm: '13pt'}}}>
-                Contact Support
-              </Button>
-            </Stack>
-          </Stack>
-        </CardContent>
-      </Card>
-    </Stack>
-  )
+      <Stack spacing={2}>
+        {/*Change to map when there is data*/}
+        <CurrentBookingCard room='K17 G02 - Consultation Room' time='7:30 PM'/>
+        <CurrentBookingCard room='K17 G02 - Consultation Room' time='7:30 PM'/>
+        <CurrentBookingCard room='K17 G02 - Consultation Room' time='7:30 PM'/>
+      </Stack>
+    </Stack>)
 }
---
+++ frontend/app/components/CurrentBookingCard.tsx
@@ -0,0 +1,35 @@
+import {Box, Button, Card, CardContent, Stack, Typography} from "@mui/joy";
+
+interface CurrentBookingProps {
+  room: string,
+  time: string
+}
+
+export default function CurrentBookingCard({room, time}: CurrentBookingProps) {
+  return (<Card variant='outlined'>
+      <CardContent>
+        <Stack direction={{xs: 'column', lg: 'row'}} px={2} py={1} width='100%' justifyContent='space-between'
+               alignItems={{xs: 'flex-start', lg: 'center'}}>
+          <Box>
+            <Typography level="h2" sx={{textWrap: 'wrap'}}>
+              {room}
+            </Typography>
+            <Typography level='body-lg' pb={1} sx={{textWrap: 'wrap'}}>
+              Booked until {time}
+            </Typography>
+          </Box>
+          <Stack direction='row' spacing={{xs: 1, sm: 4}} height={60} width={{xs: '100%', lg: '50%'}} py='10px'
+                 justifyContent='flex-end'>
+            <Button size="md" color="success"
+                    sx={{borderRadius: '20px', width: '130px', fontSize: {xs: '11pt', sm: '13pt'}}}>
+              Check in
+            </Button>
+            <Button size="md" color="danger"
+                    sx={{borderRadius: '20px', width: '180px', fontSize: {xs: '11pt', sm: '13pt'}}}>
+              Contact Support
+            </Button>
+          </Stack>
+        </Stack>
+      </CardContent>
+    </Card>)
+}
