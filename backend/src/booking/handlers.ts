758e23fc6b1a7c78ec9625cf75a6fac7adb9dc6e
Tom Pacino <tompacino@users.noreply.github.com>
2024-06-22 21:40:05 +1000

    Review changes

--- backend/src/booking/handlers.ts
+++ backend/src/booking/handlers.ts
@@ -1,94 +1,93 @@
 // Booking endpoint handlers
 
-import { DATABASE_URL, PORT } from '../../config';
-import { Pool } from 'pg';
+import { db } from '../index'
 import { sql } from "drizzle-orm"
-import { drizzle } from "drizzle-orm/node-postgres";
 import { booking } from '../../drizzle/schema';
 import { TypedRequest, TypedResponse, DateTimeRange } from '../types';
 import typia from 'typia';
 
-const pool = new Pool({
-    connectionString: DATABASE_URL,
-  });
-const db = drizzle(pool);
-
 export async function currentBookings(
-    req: TypedRequest,
-    res: TypedResponse,
-  ) {
-    try {
-        const zid = req.token.user;
-        const currentTime = new Date().toISOString();
+  req: TypedRequest,
+  res: TypedResponse,
+) {
+  try {
+    const zid = req.token.user;
+    const currentTime = new Date().toISOString();
 
-        const currentBookings = await db
-            .select()
-            .from(booking)
-            .where(
-                sql`${booking.starttime} < ${currentTime} AND ${booking.endtime} > ${currentTime} AND ${booking.zid} = ${zid}`
-            );
+    const currentBookings = await db
+      .select({
+        id: booking.id,
+        starttime: booking.starttime,
+        endttime: booking.endtime,
+        spaceId: booking.spaceid,
+        currentStatus: booking.currentstatus
+      })
+      .from(booking)
+      .where(
+        sql`${booking.starttime} < ${currentTime} AND ${booking.endtime} > ${currentTime} AND ${booking.zid} = ${zid}`
+      );
 
-        res.json({ bookings: currentBookings });
-    } catch (error) {
-        res.status(500).json({ error: 'Failed to fetch current bookings' });
-    }
+    res.json({ bookings: currentBookings });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch current bookings' });
   }
+}
 
 export async function upcomingBookings(
-    req: TypedRequest,
-    res: TypedResponse,
-  ) {
-    try {
-        const zid = req.token.user;
-        const currentTime = new Date().toISOString();
+  req: TypedRequest,
+  res: TypedResponse,
+) {
+  try {
+    const zid = req.token.user;
+    const currentTime = new Date().toISOString();
 
-        const currentBookings = await db
-            .select({
-                id: booking.id,
-                starttime: booking.starttime,
-                endttime: booking.endtime,
-                spaceId: booking.spaceid,
-                currentStatus: booking.currentstatus
-            })
-            .from(booking)
-            .where(
-                sql`${booking.starttime} > ${currentTime} AND ${booking.zid} = ${zid}`
-            );
+    const currentBookings = await db
+      .select({
+        id: booking.id,
+        starttime: booking.starttime,
+        endttime: booking.endtime,
+        spaceId: booking.spaceid,
+        currentStatus: booking.currentstatus
+      })
+      .from(booking)
+      .where(
+        sql`${booking.starttime} > ${currentTime} AND ${booking.zid} = ${zid}`
+      );
 
-        res.json({ bookings: currentBookings });
-    } catch (error) {
-        res.status(500).json({ error: 'Failed to fetch current bookings' });
-    }
+    res.json({ bookings: currentBookings });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch current bookings' });
   }
+}
 
 export async function pastBookings(
-    req: TypedRequest,
-    res: TypedResponse,
-  ) {
-    try {
-        if (!typia.is<DateTimeRange>(req.body)) {
-            res.status(400).json({ error: "Invalid input" });
-            return;
-        }
-        const zid = req.token.user;
-        const start = req.body.start;
-        const end = req.body.end;
-
-        const currentBookings = await db
-            .select({
-                id: booking.id,
-                starttime: booking.starttime,
-                endttime: booking.endtime,
-                spaceId: booking.spaceid,
-                currentStatus: booking.currentstatus
-            })
-            .from(booking)
-            .where(
-                sql`${booking.starttime} <= ${end} AND ${booking.endtime} >= ${start} AND ${booking.zid} = ${zid}`
-            );
-
-        res.json({ bookings: currentBookings });
-    } catch (error) {
-        res.status(500).json({ error: 'Failed to fetch past bookings' });
+  req: TypedRequest<DateTimeRange>,
+  res: TypedResponse,
+) {
+  try {
+    if (!typia.is<DateTimeRange>(req.body)) {
+      res.status(400).json({ error: "Invalid input" });
+      return;
     }
+    const zid = req.token.user;
+    const start = req.body.start;
+    const end = req.body.end;
+
+    const currentBookings = await db
+      .select({
+        id: booking.id,
+        starttime: booking.starttime,
+        endttime: booking.endtime,
+        spaceId: booking.spaceid,
+        currentStatus: booking.currentstatus
+      })
+      .from(booking)
+      .where(
+        sql`${booking.starttime} <= ${end} AND ${booking.endtime} >= ${start} AND ${booking.zid} = ${zid}`
+      );
+
+    res.json({ bookings: currentBookings });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch past bookings' });
   }
+}
