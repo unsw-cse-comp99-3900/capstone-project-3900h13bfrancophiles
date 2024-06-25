1c1469eef40e7fdc6bda314f4a6091a28f57d107
Franco Reyes <franco.javier.reyes@gmail.com>
2024-06-25 13:59:37 +1000

    room and hotdesk status endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,6 +9,7 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { login, logout } from './auth/handlers';
 import { validateToken } from './auth/middleware';
 import { currentBookings, upcomingBookings, pastBookings, rangeOfBookings } from './booking/handlers';
+import { spaceStatus } from './status/handlers';
 
 const pool = new Pool({
   connectionString: DATABASE_URL,
@@ -28,6 +29,8 @@ app.get("/bookings/current", validateToken, currentBookings);
 app.get("/bookings/upcoming", validateToken, upcomingBookings);
 app.get("/bookings/past", validateToken, pastBookings);
 app.get("/bookings/range", validateToken, rangeOfBookings);
+
+app.get("/status", validateToken, spaceStatus);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
