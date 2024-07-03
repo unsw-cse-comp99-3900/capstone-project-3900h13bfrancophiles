b0eaa8bc036ad231a92cdfd7e66522c7bb2b0a02
Sofia De Bellis <sofia.debellis23@gmail.com>
2024-07-03 13:04:29 +1000

    (fix): Add isLoading prop

--- frontend/hooks/useRoomDetails.ts
+++ frontend/hooks/useRoomDetails.ts
@@ -6,7 +6,7 @@ import { Room } from '@/types';
  * Hook to fetch all room details
  */
 export default function useRoomDetails() {
-  const { data, error } = useSWR<{ rooms: Room[] }>("/rooms", swrFetcher);
+  const { data, isLoading, error } = useSWR<{ rooms: Room[] }>("/rooms", swrFetcher);
 
-  return { roomsData: data?.rooms, error };
+  return { roomsData: data?.rooms, isLoading, error };
 }
