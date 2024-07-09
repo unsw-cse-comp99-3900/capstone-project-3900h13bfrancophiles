1b52ab1f3f0777b3b6443dcca04af7983a2ddcf2
Angela Wen <angela@angelas-air-2.lan>
2024-07-09 17:09:23 +1000

    typo

--- frontend/hooks/useUpcomingBookings.ts
+++ frontend/hooks/useUpcomingBookings.ts
@@ -6,6 +6,6 @@ import { Booking } from '@/types';
  * Hook to fetch upcoming bookings
  */
 export default function useUpcomingBookings(type: string) {
-  const { data, isLoading, error, mutate} = useSWR<{ bookings: Booking[] }>(`/bookings/upcoming?&type=${type}`, swrFetcher);
+  const { data, isLoading, error, mutate} = useSWR<{ bookings: Booking[] }>(`/bookings/upcoming?type=${type}`, swrFetcher);
   return { upcomingBookings: data?.bookings, isLoading, error, mutate };
 }
