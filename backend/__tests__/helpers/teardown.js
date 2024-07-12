4bca49f1ce36bf6b69da144bbfeeb6c37b92cfb2
Franco Reyes <franco.javier.reyes@gmail.com>
2024-07-12 20:11:13 +1000

    Split up tests by route

--- backend/__tests__/helpers/teardown.js
+++ backend/__tests__/helpers/teardown.js
@@ -3,6 +3,7 @@ import { execSync } from 'child_process';
 module.exports = async function (globalConfig, projectConfig) {
   // Kill backend server
   process.kill(globalThis.__server__.pid, 'SIGTERM');
+  console.log("Closed backend server - logs available in __tests__/server.log");
 
   execSync("./__tests__/helpers/db_teardown.sh");
 };
