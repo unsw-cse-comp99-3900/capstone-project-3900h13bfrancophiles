46e46ff708ee5530e8c1454da9eac92fc92f903f
Franco Reyes <franco.javier.reyes@gmail.com>
2024-07-12 20:49:53 +1000

    print out server log

--- backend/__tests__/helpers/teardown.js
+++ backend/__tests__/helpers/teardown.js
@@ -1,9 +1,12 @@
 import { execSync } from 'child_process';
+import * as fs from 'fs';
 
 module.exports = async function (globalConfig, projectConfig) {
   // Kill backend server
   process.kill(globalThis.__server__.pid, 'SIGTERM');
   console.log("Closed backend server - logs available in __tests__/server.log");
 
-  execSync("./__tests__/helpers/db_teardown.sh");
+  console.log(fs.readFileSync('./__tests__/server.log', 'utf8'))
+
+  // execSync("./__tests__/helpers/db_teardown.sh");
 };
