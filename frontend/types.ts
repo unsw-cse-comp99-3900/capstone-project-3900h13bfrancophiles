6b117d17f4b7f5e70639c8d995c3dac935c90216
3syth <120223314+3syth@users.noreply.github.com>
2024-07-12 20:26:30 +1000

    Francophil 89 - Show Bookings on floorplan (#44)
    desk interaction and user image display

--- frontend/types.ts
+++ frontend/types.ts
@@ -6,4 +6,9 @@ export type Desk = { id: string, name: string, floor: string, room: string, desk
 
 export type Space = Room | Desk;
 
+export type Status = { status: "available" }
+  | { status: "unavailable", booking: Booking };
+
+export type User = null | { name: string, image: string }
+
 export type SpaceOption = { name: string; id: string; isRoom: boolean };
