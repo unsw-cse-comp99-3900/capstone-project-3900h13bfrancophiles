72d17d46b49d33de26bf813cb98a2d53d52f6242
Sofia De Bellis <63540422+sofiadebellis@users.noreply.github.com>
2024-07-13 14:10:34 +1000

    FRANCOPHIL-119: FE populate room status (#52)
    * (feat): Fetch room status
    
    * (fix): change invalid colour
    
    * (fix): remove typo
    
    * (fix): remove typo
    
    * Update page.tsx

--- frontend/types.ts
+++ frontend/types.ts
@@ -1,8 +1,30 @@
-export type Booking = { id: number, zid: number, starttime: string, endtime: string, spaceid: string, currentstatus: string, description: string, checkintime: string | null, checkouttime: string | null };
+export type Booking = {
+  id: number;
+  zid: number;
+  starttime: string;
+  endtime: string;
+  spaceid: string;
+  currentstatus: string;
+  description: string;
+  checkintime: string | null;
+  checkouttime: string | null;
+};
 
-export type Room = { id: string, name: string, type: string, capacity: number, roomnumber: string };
+export type Room = {
+  id: string;
+  name: string;
+  type: string;
+  capacity: number;
+  roomnumber: string;
+};
 
-export type Desk = { id: string, name: string, floor: string, room: string, desknumber: number };
+export type Desk = {
+  id: string;
+  name: string;
+  floor: string;
+  room: string;
+  desknumber: number;
+};
 
 export type Space = Room | Desk;
 
@@ -22,3 +44,11 @@ export type BookingUser =   {
   usergrp: "other" | "hdr" | "csestaff" | "admin";
 }
 export type SpaceOption = { name: string; id: string; isRoom: boolean };
+
+type Status =
+  | { status: "Available" }
+  | { status: "Unavailable"; booking: Booking };
+
+export type StatusResponse = {
+  [spaceId: string]: Status;
+};
