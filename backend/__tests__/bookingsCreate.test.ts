cfa9ce07a1b803bda847acd06c1885bc0b089bfa
Franco Reyes <franco.javier.reyes@gmail.com>
2024-07-15 16:23:24 +1000

    Test stubs

---
+++ backend/__tests__/bookingsCreate.test.ts
@@ -0,0 +1,35 @@
+
+describe("/bookings/create", () => {
+  test("Success - immediately booked", async () => {
+  });
+
+  test("Success - pending booking request", async () => {
+  });
+
+  test("Failure - non-existent space", async () => {
+  });
+
+  test("Failure - no permission to request space", async () => {
+  });
+
+  test("Failure - time already booked", async () => {
+  });
+
+  test("Failure - booking in the past", async () => {
+  });
+
+  test("Failure - end not after start", async () => {
+  });
+
+  test("Failure - booking not a multiple of 15 minutes", async () => {
+  });
+
+  test("Failure - booking not on 15 minute boundaries", async () => {
+  });
+
+  test("Failure - booking across midnight", async () => {
+  });
+
+  test("Failure - booking far in the future", async () => {
+  });
+});
