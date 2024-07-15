ebbc287e6330667999a178dbf00497c166ac5cb3
Franco Reyes <franco.javier.reyes@gmail.com>
2024-07-15 23:30:30 +1000

    Exit server gracefully

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -75,7 +75,14 @@ app.get("/admin/bookings/pending", validateToken, authoriseAtLeast("admin"), pen
 
 app.get("/users/:zid", validateToken, userDetails);
 
-
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+const closeServer = () => {
+  server.close(err => err && console.log(`${err}`));
+  server.closeAllConnections();
+}
+
+process.on("SIGTERM", closeServer);
+process.on("SIGINT", closeServer);
