cdcf505740e759334550b5f7e5a906445fb0f310
Franco Reyes <franco.javier.reyes@gmail.com>
2024-07-15 23:41:22 +1000

    Generate coverage report

--- backend/__tests__/helpers/teardown.js
+++ backend/__tests__/helpers/teardown.js
@@ -5,7 +5,9 @@ module.exports = async function (globalConfig, projectConfig) {
   process.kill(globalThis.__server__.pid, 'SIGTERM');
   fs.close(globalThis.__out__);
   fs.close(globalThis.__err__);
-  console.log("Closed backend server - logs available in __tests__/server.log");
+  console.log("Server logs available at ./__tests__/server.log");
+  console.log("Coverage report available at ./coverage/lcov-report/index.html");
+  console.log("Closing backend server...");
 
   // Close db
   await globalThis.__pgclient__.end();
