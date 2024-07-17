1d3ae005f848f52b1350b4d6d24a17183fa4bb93
Sofia De Bellis <sofia.debellis23@gmail.com>
2024-07-17 18:18:31 +1000

    (chore): getting ready to pull

--- frontend/components/CurrentBookings.tsx
+++ frontend/components/CurrentBookings.tsx
@@ -1,43 +1,39 @@
 "use client";
-
+import * as React from "react";
 import {
   Box,
   Button,
-  Card,
-  CardContent,
   DialogActions,
   DialogContent,
   DialogTitle,
   Divider,
   Modal,
   ModalDialog,
+  Option,
+  Select,
+  Sheet,
   Skeleton,
   Stack,
+  Table,
   Typography,
+  Link,
 } from "@mui/joy";
-import { Booking } from "@/types";
-import useSpace from "@/hooks/useSpace";
-import useCurrentBookings from "@/hooks/useCurrentBookings";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import WarningRoundedIcon from "@mui/icons-material/WarningRounded";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
 import { format } from "date-fns";
 import { useState } from "react";
 import { checkIn, checkOut } from "@/api";
 import { mutate } from 'swr';
 
-function CurrentBookings() {
-  const { currentBookings } = useCurrentBookings();
+import { NoBookingsRow } from '@/components/NoBookingsRow';
+import { Booking } from '@/types';
 
-  return currentBookings?.length ? (
-    <>
-      <Typography level="h2" mb={2}>
-        Current Booking{currentBookings.length > 1 && "s"}
-      </Typography>
-      <Stack spacing={2}>
-        {currentBookings.map((booking) => (
-          <CurrentBookingCard key={booking.id} booking={booking} />
-        ))}
-      </Stack>
-    </>
-  ) : null;
+export interface UpcomingBookingRowProps {
+  row: Booking;
+  mutate: () => void; // Pass mutate function as prop
 }
 
 interface CurrentBookingCardProps {
@@ -89,6 +85,8 @@ function CurrentBookingCard({ booking }: CurrentBookingCardProps) {
       setIsCheckingInOrOut(true);
       setCheckInOrOutError(null);
 
+  const handleDelete = async () => {
+    if (bookingToDelete !== null) {
       try {
         await checkIn(booking.id);
         setCheckedIn(true);
@@ -107,36 +105,43 @@ function CurrentBookingCard({ booking }: CurrentBookingCardProps) {
     }
   };
 
+  const handleOpenModal = (id: number) => {
+    setBookingToDelete(id);
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setBookingToDelete(null);
+    setModalOpen(false);
+  };
+
+  const handleOpenEditModal = () => {
+    setEditModalOpen(true);
+  };
+
+  const handleCloseEditModal = () => {
+    setEditModalOpen(false);
+    mutate();
+  };
+
   return (
     <>
-      <Card variant="outlined">
-        <CardContent>
-          <Stack
-            direction={{ xs: "column", lg: "row" }}
-            px={2}
-            py={1}
-            width="100%"
-            justifyContent="space-between"
-            alignItems={{ xs: "flex-start", lg: "center" }}
-          >
-            <Box>
-              <Typography level="h3" sx={{ textWrap: "wrap" }}>
-                <Skeleton loading={isLoading}>{space?.name}</Skeleton>
-              </Typography>
-              <Typography level="body-lg" pb={1} sx={{ textWrap: "wrap" }}>
-                <Skeleton loading={isLoading}>
-                  Booked {format(new Date(booking.starttime), "p")} -{" "}
-                  {format(new Date(booking.endtime), "p")}
-                </Skeleton>
-              </Typography>
-            </Box>
-            <Stack
-              direction="row"
-              spacing={{ xs: 1, sm: 4 }}
-              height={60}
-              width={{ xs: "100%", lg: "50%" }}
-              py="10px"
-              justifyContent="flex-end"
+      <tr>
+        <td>
+          <BookingStatusPill status={row.currentstatus} />
+        </td>
+        <td>
+          <Typography level="body-sm">
+            {format(new Date(row.starttime), "dd/MM/yy H:mm")} -{" "}
+            {format(new Date(row.endtime), "H:mm")}
+          </Typography>
+        </td>
+        <td>
+          <Skeleton loading={isLoading}>
+            <Link
+              href={type === 'room' ? `/rooms/${row.spaceid}` : `/desks/${row.spaceid}`}
+              level="body-sm"
+              component={NextLink}
             >
               <Button
                 size="sm"
@@ -189,8 +194,174 @@ function CurrentBookingCard({ booking }: CurrentBookingCardProps) {
           </DialogActions>
         </ModalDialog>
       </Modal>
+      <BookingModal
+        open={editModalOpen}
+        onClose={() => handleCloseEditModal()}
+        space={space ? { id: space!.id, name: space!.name, isRoom: type === 'room' } : undefined}
+        date={new Date(row.starttime)}
+        start={new Date(row.starttime)}
+        end={new Date(row.endtime)}
+        desc={row.description}
+        editing
+        editedBooking={row.id}
+        // I think reapproval stuff is handled by backend so don't need to pass status?
+      />
     </>
   );
 }
 
-export default CurrentBookings;
+export default function UpcomingBookings() {
+  const [sort, setSort] = React.useState('soonest');
+  const [filter, setFilter] = React.useState("all");
+  const { upcomingBookings, isLoading, mutate } = useUpcomingBookings(filter, sort); // Get mutate function from hook
+
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleChangePage = (newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: any, newValue: number | null) => {
+    setRowsPerPage(parseInt(newValue!.toString(), 10));
+    setPage(0);
+  };
+
+  function labelDisplayedRows(from: number, to: number, count: number) {
+    return `${from}–${to} of ${count}`;
+  }
+
+  const handleChangeFilter = (event: any, newValue: string | null) => {
+    if (newValue !== null) {
+      setFilter(newValue);
+    }
+  };
+
+  const handleChangeSort = (event: React.SyntheticEvent | null, newValue: string | null) => {
+    if (newValue !== null) {
+      setSort(newValue);
+    }
+  };
+
+  const getLabelDisplayedRowsTo = () => {
+    return Math.min(upcomingBookings?.length ?? 0, (page + 1) * rowsPerPage);
+  };
+
+  const numColumns = 5;
+
+  return (<Stack>
+      <Stack direction="row" width="100%" my={1} spacing={1}>
+        <Box width="150px">
+          Space
+          <Select
+            defaultValue="all"
+            placeholder="All"
+            onChange={handleChangeFilter}
+          >
+            <Option value="all">All</Option>
+            <Option value="rooms">Rooms</Option>
+            <Option value="desks">Desks</Option>
+          </Select>
+        </Box>
+        <Box width="150px">
+          Sort
+          <Select defaultValue="soonest" placeholder="Soonest" onChange={handleChangeSort}>
+            <Option value="soonest">Soonest</Option>
+            <Option value="latest">Latest</Option>
+          </Select>
+        </Box>
+      </Stack>
+      <Sheet
+        variant="outlined"
+        sx={{
+          display: {xs: "initial"}, width: "100%", borderRadius: "sm", flexShrink: 1, overflow: "auto", minHeight: 0,
+        }}
+      >
+        <Table
+          aria-labelledby="tableTitle"
+          stickyHeader
+          hoverRow={!!upcomingBookings?.length}
+          sx={{
+            "--TableCell-headBackground": "var(--joy-palette-background-level1)",
+            "--Table-headerUnderlineThickness": "1px",
+            "--TableRow-hoverBackground": "var(--joy-palette-background-level1)",
+            "--TableCell-paddingY": "4px",
+            "--TableCell-paddingX": "8px",
+          }}
+        >
+          <thead>
+          <tr>
+            <th style={{width: 100, padding: "12px 6px"}}>Status</th>
+            <th style={{width: 140, padding: "12px 6px"}}>Time</th>
+            <th style={{width: 140, padding: "12px 6px"}}>Location</th>
+            <th style={{width: 150, padding: "12px 6px"}}>Description</th>
+            <th style={{width: 100, padding: "12px 6px"}}></th>
+          </tr>
+          </thead>
+          <tbody>
+            {!!upcomingBookings?.length
+              ? upcomingBookings
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row) => (
+                  <UpcomingBookingRow key={row.id} row={row} mutate={mutate} />
+                ))
+              : <NoBookingsRow bookingType='Upcoming' colSpan={numColumns} isLoading={isLoading} />
+            }
+          </tbody>
+          <tfoot>
+          <tr>
+            <td colSpan={numColumns}>
+              <Box
+                sx={{
+                  display: "flex", alignItems: "center", gap: 2, justifyContent: "flex-end",
+                }}
+              >
+                <FormControl orientation="horizontal" size="sm">
+                  <FormLabel>Rows per page:</FormLabel>
+                  <Select
+                    onChange={handleChangeRowsPerPage}
+                    placeholder="5"
+                    value={rowsPerPage}
+                  >
+                    <Option value={5}>5</Option>
+                    <Option value={10}>10</Option>
+                    <Option value={25}>25</Option>
+                  </Select>
+                </FormControl>
+                <Typography textAlign="center" sx={{minWidth: 80}}>
+                  {labelDisplayedRows(
+                    upcomingBookings ? page * rowsPerPage + 1 : 0,
+                    getLabelDisplayedRowsTo(),
+                    upcomingBookings?.length ?? 0,
+                  )}
+                </Typography>
+                <Box sx={{display: "flex", gap: 1}}>
+                  <IconButton
+                    size="sm"
+                    color="neutral"
+                    variant="outlined"
+                    disabled={page === 0}
+                    onClick={() => handleChangePage(page - 1)}
+                    sx={{bgcolor: "background.surface"}}
+                  >
+                    <KeyboardArrowLeftIcon/>
+                  </IconButton>
+                  <IconButton
+                    size="sm"
+                    color="neutral"
+                    variant="outlined"
+                    disabled={page >= Math.ceil((upcomingBookings?.length ?? 0) / rowsPerPage) - 1}
+                    onClick={() => handleChangePage(page + 1)}
+                    sx={{bgcolor: "background.surface"}}
+                  >
+                    <KeyboardArrowRightIcon/>
+                  </IconButton>
+                </Box>
+              </Box>
+            </td>
+          </tr>
+          </tfoot>
+        </Table>
+      </Sheet>
+    </Stack>);
+}
