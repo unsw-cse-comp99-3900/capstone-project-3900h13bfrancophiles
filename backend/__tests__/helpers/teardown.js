5b954fa4d583370a75eccba2165b017ddd254e91
Franco Reyes <franco.javier.reyes@gmail.com>
2024-07-18 14:12:41 +1000

    More reliable teardown

--- backend/__tests__/helpers/teardown.js
+++ backend/__tests__/helpers/teardown.js
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 
 module.exports = async function (globalConfig, projectConfig) {
   // Kill backend server
-  process.kill(globalThis.__server__.pid, 'SIGTERM');
+  globalThis.__server__.kill('SIGTERM');
   fs.close(globalThis.__out__);
   fs.close(globalThis.__err__);
   console.log("Server logs available at ./__tests__/server.log");
