d46c516823550aa6556f2f645333aa2c23b8f973
Franco Reyes <franco.javier.reyes@gmail.com>
2024-07-20 10:12:46 +1000

    Copy JoyUI Date field from docs

--- frontend/app/ThemeRegistry.tsx
+++ frontend/app/ThemeRegistry.tsx
@@ -19,40 +19,6 @@ type ThemeRegistryProps = {
 export default function ThemeRegistry({
   children
 }: ThemeRegistryProps) {
-
-  // default font family is Inter
-  const theme = extendTheme({
-    typography: {
-      // defining custom levels
-      h1: {
-        color: '#33373D',
-        margin: '30px 0px'
-      },
-      h2: {
-        color: '#33373D',
-      },
-      h3: {
-        color: '#33373D',
-
-      },
-      body: {
-        color: '#33373D',
-      },
-      'title-lg': {
-        color: '#33373D',
-      },
-      'body-sm': {
-        color: '#33373D',
-      },
-      'body-md': {
-        color: '#33373D',
-      },
-      'body-lg': {
-        color: '#33373D',
-      },
-    },
-  });
-
   return (
     <CssVarsProvider theme={theme}>
       <CssBaseline />
@@ -60,3 +26,36 @@ export default function ThemeRegistry({
     </CssVarsProvider>
   );
 }
+
+// default font family is Inter
+export const theme = extendTheme({
+  typography: {
+    // defining custom levels
+    h1: {
+      color: '#33373D',
+      margin: '30px 0px'
+    },
+    h2: {
+      color: '#33373D',
+    },
+    h3: {
+      color: '#33373D',
+
+    },
+    body: {
+      color: '#33373D',
+    },
+    'title-lg': {
+      color: '#33373D',
+    },
+    'body-sm': {
+      color: '#33373D',
+    },
+    'body-md': {
+      color: '#33373D',
+    },
+    'body-lg': {
+      color: '#33373D',
+    },
+  },
+});
