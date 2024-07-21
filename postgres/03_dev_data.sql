360609da491f68afefeeb9cefb846c004991c847
angewen <119286195+angewen@users.noreply.github.com>
2024-07-21 22:21:47 +1000

    db update (#80)
    Co-authored-by: Angela Wen <angela@angelas-air-3.lan>

--- postgres/03_dev_data.sql
+++ postgres/03_dev_data.sql
@@ -88,37 +88,37 @@ INSERT INTO booking (zId, startTime, endTime, spaceId, currentStatus, descriptio
     (1234567, '2024-01-14T13:30', '2024-01-014T14:30', 'K-K17-B01', 'confirmed', 'meeting'),
 
     -- Admin upcoming room bookings
-    (1234567, '2024-07-20T10:30', '2024-07-20T11:30', 'K-K17-B01', 'confirmed', 'class'),
-    (1234567, '2024-07-21T11:30', '2024-07-21T12:30', 'K-K17-B01', 'confirmed', 'studying'),
+    (1234567, '2024-07-25T10:30', '2024-07-25T11:30', 'K-K17-B01', 'confirmed', 'class'),
+    (1234567, '2024-07-25T11:30', '2024-07-25T12:30', 'K-K17-B01', 'confirmed', 'studying'),
 
     -- Admin upcoming desk bookings
-    (1234567, '2024-07-20T03:00', '2024-07-20T05:00', 'K-K17-301-11', 'confirmed', 'studying'),
+    (1234567, '2024-07-25T03:00', '2024-07-25T05:00', 'K-K17-301-11', 'confirmed', 'studying'),
 
     --------------------------------------------------------------------------------------------
     -- HDR upcoming room bookings
-    (7654321, '2024-07-22T16:30', '2024-07-22T17:30', 'K-K17-402', 'pending', 'meeting'),
-    (7654321, '2024-07-23T16:30', '2024-07-23T17:30', 'K-K17-402', 'pending', 'event'),
-    (7654321, '2024-07-24T16:30', '2024-07-24T17:30', 'K-K17-402', 'pending', 'studying'),
-    (7654321, '2024-07-25T16:30', '2024-07-25T17:30', 'K-K17-402', 'declined', 'workshop'),
+    (7654321, '2024-08-22T16:30', '2024-08-22T17:30', 'K-K17-402', 'pending', 'meeting'),
+    (7654321, '2024-08-23T16:30', '2024-08-23T17:30', 'K-K17-402', 'pending', 'event'),
+    (7654321, '2024-08-24T16:30', '2024-08-24T17:30', 'K-K17-402', 'pending', 'studying'),
+    (7654321, '2024-08-25T16:30', '2024-08-25T17:30', 'K-K17-402', 'declined', 'workshop'),
 
     -- HDR upcoming desk bookings
-    (7654321, '2024-07-19T03:00', '2024-07-19T05:00', 'K-K17-201-2', 'confirmed', 'studying'),
-    (7654321, '2024-07-20T03:00', '2024-07-20T05:00', 'K-K17-201-3', 'confirmed', 'doing research'),
-    (7654321, '2024-07-21T03:00', '2024-07-21T05:00', 'K-K17-201-1', 'confirmed', 'studying'),
-    (7654321, '2024-07-22T03:00', '2024-07-22T05:00', 'K-K17-201-1', 'confirmed', 'doing research'),
-    (7654321, '2024-07-23T03:00', '2024-07-23T05:00', 'K-K17-201-3', 'confirmed', 'studying'),
-    (7654321, '2024-07-24T03:00', '2024-07-24T05:00', 'K-K17-201-2', 'confirmed', 'doing research'),
+    (7654321, '2024-08-19T03:00', '2024-08-19T05:00', 'K-K17-201-2', 'confirmed', 'studying'),
+    (7654321, '2024-08-20T03:00', '2024-08-20T05:00', 'K-K17-201-3', 'confirmed', 'doing research'),
+    (7654321, '2024-08-21T03:00', '2024-08-21T05:00', 'K-K17-201-1', 'confirmed', 'studying'),
+    (7654321, '2024-08-22T03:00', '2024-08-22T05:00', 'K-K17-201-1', 'confirmed', 'doing research'),
+    (7654321, '2024-08-23T03:00', '2024-08-23T05:00', 'K-K17-201-3', 'confirmed', 'studying'),
+    (7654321, '2024-08-24T03:00', '2024-08-24T05:00', 'K-K17-201-2', 'confirmed', 'doing research'),
 
     --------------------------------------------------------------------------------------------
-    -- DEMO B BOOKINGS
-    -- Current bookings- wk 8 thursday 12pm - 4pm SYD time
-    (1234567, '2024-07-18T02:00', '2024-07-18T06:00', 'K-K17-201-2', 'confirmed', 'current desk booking demo B'),
-    (7654321, '2024-07-18T02:00', '2024-07-18T06:00', 'K-J17-504', 'confirmed', 'current booking demo B'),
-    (7654322, '2024-07-18T02:00', '2024-07-18T06:00', 'K-K17-201-3', 'confirmed', 'current desk booking demo B'),
-    (7654333, '2024-07-18T02:00', '2024-07-18T06:00', 'K-K17-301-15', 'confirmed', 'current desk booking demo B'),
-    (7654444, '2024-07-18T02:00', '2024-07-18T06:00', 'K-K17-301-16', 'confirmed', 'current desk booking demo B'),
-    (7655555, '2024-07-18T02:00', '2024-07-18T06:00', 'K-K17-301-12', 'confirmed', 'current desk booking demo B'),
-    (7666666, '2024-07-18T02:00', '2024-07-18T06:00', 'K-K17-201-4', 'confirmed', 'current desk booking demo B');
+    -- DEMO BOOKINGS
+    -- Current bookings- monday 10am - 4pm SYD time
+    (1234567, '2024-07-22T00:00', '2024-07-22T06:00', 'K-K17-201-2', 'confirmed', 'current desk booking demo'),
+    (7654321, '2024-07-22T00:00', '2024-07-22T06:00', 'K-J17-504', 'confirmed', 'current booking demo'),
+    (7654322, '2024-07-22T00:00', '2024-07-22T06:00', 'K-K17-201-3', 'confirmed', 'current desk booking demo'),
+    (7654333, '2024-07-22T00:00', '2024-07-22T06:00', 'K-K17-301-15', 'confirmed', 'current desk booking demo'),
+    (7654444, '2024-07-22T00:00', '2024-07-22T06:00', 'K-K17-301-16', 'confirmed', 'current desk booking demo'),
+    (7655555, '2024-07-22T00:00', '2024-07-22T06:00', 'K-K17-301-12', 'confirmed', 'current desk booking demo'),
+    (7666666, '2024-07-22T00:00', '2024-07-22T06:00', 'K-K17-201-4', 'confirmed', 'current desk booking demo');
 
 
 
