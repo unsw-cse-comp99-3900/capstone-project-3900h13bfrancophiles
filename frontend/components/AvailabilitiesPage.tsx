4019395c757b40cc1467406f703966222e5a2670
Ben <bengodw@gmail.com>
2024-07-25 12:19:05 +1000

    feat: room headings grid for mobile

--- frontend/components/AvailabilitiesPage.tsx
+++ frontend/components/AvailabilitiesPage.tsx
@@ -1,6 +1,6 @@
 
 import * as React from "react";
-import { Stack, Typography, Button, Grid, Item } from "@mui/joy";
+import { Stack, Typography, Button, Grid, Sheet } from "@mui/joy";
 import useSpace from '@/hooks/useSpace';
 import useAvailabilities from "@/hooks/useAvailabilities";
 import { Room, Desk } from "@/types";
@@ -8,11 +8,22 @@ import Loading from "@/components/Loading";
 import AvailabilityCalendar from "@/components/AvailabilityCalendar";
 import BookingModal from "@/components/BookingModal/BookingModal"
 import Error from "@/components/Error";
+import { styled } from '@mui/joy/styles';
 
 interface AvailabilitesPageProps {
   spaceId: string;
   spaceType: string;
 }
+
+const Item = styled(Sheet)(({ theme }) => ({
+  backgroundColor:
+    theme.palette.mode === 'dark' ? theme.palette.background.level1 : '#fff',
+  ...theme.typography['body-sm'],
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  borderRadius: 4,
+  color: theme.vars.palette.text.secondary,
+}));
 
 const AvailabilitiesPage : React.FC<AvailabilitesPageProps> = ({
   spaceId,
@@ -71,12 +82,14 @@ const AvailabilitiesPage : React.FC<AvailabilitesPageProps> = ({
         </Stack>
         {spaceType === "room" &&
           <>
+            {/* display for desktop */}
             <Stack
               direction="row"
               justifyContent="flex-start"
               gap={5}
               borderRadius="sm"
               mb={3}
+              sx={{ display: {xs: "none", sm: "flex"}}}
             >
               <Stack>
                 <Typography level="h4" sx={{ color: "gray" }}>
@@ -103,24 +116,45 @@ const AvailabilitiesPage : React.FC<AvailabilitesPageProps> = ({
                 </Typography>
               </Stack>
             </Stack>
-            <Grid>
-              <Grid xs={6}>
-                <Item>hi </Item>
+            {/* dislplay for mobile */}
+            <Grid
+              container
+              spacing={1}
+              sx={{
+                flexGrow: 1,
+                display: {xs: "flex", sm: "none"}
+              }}
+              mb={2}
+            >
+              <Grid xs={4}>
+                <Typography level="h4" sx={{ color: "gray" }}>
+                  Room ID
+                </Typography>
               </Grid>
-              <Grid xs={6}>
-                hi
+              <Grid xs={8}>
+                <Typography level="h3">
+                  {room?.id}
+                </Typography>
               </Grid>
-              <Grid xs={6}>
-                hi
+              <Grid xs={4}>
+                <Typography level="h4" sx={{ color: "gray" }}>
+                  Usage
+                </Typography>
               </Grid>
-              <Grid xs={6}>
-                hi
+              <Grid xs={8}>
+                <Typography level="h3">
+                  {room?.type}
+                </Typography>
               </Grid>
-              <Grid xs={6}>
-                hi
+              <Grid xs={4}>
+                <Typography level="h4" sx={{ color: "gray" }}>
+                  Capacity
+                </Typography>
               </Grid>
-              <Grid xs={6}>
-                hi
+              <Grid xs={8}>
+                <Typography level="h3">
+                  {room?.capacity}
+                </Typography>
               </Grid>
             </Grid>
           </>
