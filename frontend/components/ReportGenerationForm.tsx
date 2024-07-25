2fdad5dbc21764682a066f9933fab0f13ef5693f
Franco Reyes <franco.javier.reyes@gmail.com>
2024-07-25 21:53:19 +1000

    Forgot to make the report types controlled

--- frontend/components/ReportGenerationForm.tsx
+++ frontend/components/ReportGenerationForm.tsx
@@ -4,7 +4,10 @@ import {
   Autocomplete,
   AutocompleteOption,
   AutocompleteRenderGroupParams,
+  Box,
   Button,
+  Chip,
+  ChipDelete,
   FormControl,
   FormLabel,
   Input,
@@ -19,33 +22,52 @@ import {
 } from "@mui/joy";
 import DownloadIcon from "@mui/icons-material/Download";
 import * as React from "react";
-import { AutocompleteRenderOptionState } from "@mui/joy/Autocomplete/AutocompleteProps";
+import { useRef } from "react";
+import { AutocompleteRenderGetTagProps, AutocompleteRenderOptionState } from "@mui/joy/Autocomplete/AutocompleteProps";
 import { addDays, format, startOfToday } from "date-fns";
 import { useResizeObserver } from "usehooks-ts";
-import { useRef } from "react";
 
-interface OptionType {
+interface Option {
   text: string;
   value: string;
   type: "room" | "desk";
   level?: string;
 }
 
-export function ReportGenerationForm() {
-  const options: OptionType[] = [
-    { text: "All Desks", value: "all", type: "desk" },
-    { text: "All Rooms", value: "all", type: "room" },
-    { text: "K17 201-B", value: "K-K17-201B", type: "room", level: "K17 L2" },
-    { text: "K17 201 Desks", value: "K-K17-201", type: "desk", level: "K17 L2" },
-    { text: "K17 217 Desks", value: "K-K17-217", type: "desk", level: "K17 L2" },
-    { text: "K17 401 K", value: "K-K17-401K", type: "room", level: "K17 L4" },
-    { text: "K17 402", value: "K-K17-402", type: "room", level: "K17 L4" },
-    { text: "K17 403", value: "K-K17-403", type: "room", level: "K17 L4" },
-    { text: "K17 401 Desks", value: "K-K17-401", type: "desk", level: "K17 L4" },
-    { text: "K17 412 Desks", value: "K-K17-412", type: "desk", level: "K17 L4" },
-  ];
+// TODO: Fetch from backend
+const options: Option[] = [
+  { text: "All Desks", value: "all", type: "desk" },
+  { text: "All Rooms", value: "all", type: "room" },
+  { text: "K17 201-B", value: "K-K17-201B", type: "room", level: "K17 L2" },
+  { text: "K17 201 Desks", value: "K-K17-201", type: "desk", level: "K17 L2" },
+  { text: "K17 217 Desks", value: "K-K17-217", type: "desk", level: "K17 L2" },
+  { text: "K17 401 K", value: "K-K17-401K", type: "room", level: "K17 L4" },
+  { text: "K17 402", value: "K-K17-402", type: "room", level: "K17 L4" },
+  { text: "K17 403", value: "K-K17-403", type: "room", level: "K17 L4" },
+  { text: "K17 401 Desks", value: "K-K17-401", type: "desk", level: "K17 L4" },
+  { text: "K17 412 Desks", value: "K-K17-412", type: "desk", level: "K17 L4" },
+];
 
-  const [spaces, setSpaces] = React.useState<OptionType[]>([]);
+interface ReportType {
+  type: string;
+  name: string;
+  supportedFormats: string[];
+}
+
+// TODO: Fetch from backend?
+const reportTypes: ReportType[] = [
+  { type: "booking", name: "Booking Information", supportedFormats: ["xlsx"] },
+  { type: "checkin", name: "Booking & Check-in Information", supportedFormats: ["xlsx"] },
+];
+
+export function ReportGenerationForm() {
+  const [reportType, setReportType] = React.useState<ReportType>();
+
+  const today = startOfToday();
+  const [startDate, setStartDate] = React.useState(addDays(today, -7));
+  const [endDate, setEndDate] = React.useState(today);
+
+  const [spaces, setSpaces] = React.useState<Option[]>([]);
   const totals = {
     room: options.filter(space => space.type === "room").length - 1,
     desk: options.filter(space => space.type === "desk").length - 1,
@@ -55,11 +77,7 @@ export function ReportGenerationForm() {
     desk: spaces.filter(space => space.type === "desk").length,
   };
 
-  const today = startOfToday();
-  const [startDate, setStartDate] = React.useState(addDays(today, -7));
-  const [endDate, setEndDate] = React.useState(today);
-
-  const handleAllSwitch = (type: OptionType["type"]) => {
+  const handleAllSwitch = (type: Option["type"]) => {
     if (totals[type] == selected[type]) {
       setSpaces(spaces => spaces.filter(space => space.type !== type));
     } else {
@@ -78,9 +96,12 @@ export function ReportGenerationForm() {
 
   const renderOption = (
     props: Omit<React.HTMLAttributes<HTMLLIElement>, "color">,
-    option: OptionType,
+    option: Option,
     _state: AutocompleteRenderOptionState,
   ) => {
+    // @ts-ignore - key is passed in but the type doesn't include it for some reason
+    const { key, ...otherProps } = props;
+
     if (!option.level) {
       return (
         <Stack key={option.text} direction="row" width="100%" justifyContent="space-between" p={1.5} py={1}>
@@ -93,7 +114,7 @@ export function ReportGenerationForm() {
       );
     } else {
       return (
-        <AutocompleteOption key={option.value} {...props}>
+        <AutocompleteOption key={key} {...otherProps}>
           {option.text}
         </AutocompleteOption>
       );
@@ -109,12 +130,15 @@ export function ReportGenerationForm() {
       spacing={2}
     >
       <Stack direction={{ xs: "column", sm: "row" }} spacing={2}>
-        <FormControl sx={{
-          minWidth: 300,
-          flexGrow: { xs: 1, sm: 0 },
-        }}>
+        <FormControl sx={{ minWidth: 300, flexGrow: { xs: 1, sm: 0 } }}>
           <FormLabel>Report Type</FormLabel>
-          <Select defaultValue="booking">
+          <Select
+            placeholder="Select one..."
+            value={reportType?.type}
+            onChange={(_e, value) => {
+              setReportType(reportTypes.find((rt) => rt.type === value));
+            }}
+          >
             <Option value="booking">Booking Information</Option>
             <Option value="checkin">Booking & Check-in Information</Option>
           </Select>
@@ -130,7 +154,7 @@ export function ReportGenerationForm() {
                 setStartDate(new Date(e.target.value));
               }}
               slotProps={{
-                input: { max: format(endDate, "yyyy-MM-dd") }
+                input: { max: format(endDate, "yyyy-MM-dd") },
               }}
             />
           </FormControl>
@@ -147,36 +171,38 @@ export function ReportGenerationForm() {
                 input: {
                   min: format(startDate, "yyyy-MM-dd"),
                   max: format(today, "yyyy-MM-dd"),
-                }
+                },
               }}
             />
           </FormControl>
         </Stack>
       </Stack>
-      <FormControl sx={{ width: "100%" }}>
-        <FormLabel>Spaces</FormLabel>
+      <Box sx={{ width: "100%" }}>
+        <FormLabel sx={{ marginBottom: "6px" }}>Spaces</FormLabel>
         <Autocomplete
+          placeholder={spaces.length ? undefined : "Select spaces to include in report..."}
           multiple
           disableCloseOnSelect
-          limitTags={Math.floor((width ?? 0) / 150)}
+          limitTags={width ? Math.floor((width - 80) / 150) : -1}
           options={options}
           groupBy={(option) => option.level ?? ""}
           getOptionLabel={(option) => option.text}
           renderGroup={renderGroup}
           renderOption={renderOption}
+          renderTags={renderTags}
           value={spaces}
           onChange={(_e, values) => setSpaces(values)}
           slotProps={{
-            root: { ref: autocompleteRef }
+            root: { ref: autocompleteRef },
           }}
         />
-      </FormControl>
+      </Box>
       <Button
         endDecorator={<DownloadIcon />}
         size="lg"
         sx={{
           mt: "20px !important",
-          flexShrink: 0
+          flexShrink: 0,
         }}
       >
         Download Report
@@ -185,6 +211,7 @@ export function ReportGenerationForm() {
   );
 }
 
+// Stolen from JoyUI Autocomplete source code
 const renderGroup = (params: AutocompleteRenderGroupParams) => {
   return (
     <ListItem key={params.key} nested>
@@ -195,3 +222,24 @@ const renderGroup = (params: AutocompleteRenderGroupParams) => {
     </ListItem>
   );
 };
+
+// Stolen from JoyUI Autocomplete source code
+const renderTags = (
+  tags: Option[],
+  getTagProps: AutocompleteRenderGetTagProps,
+) => (
+  tags.map((item, index) => {
+    const { key, onClick, ...tagProps } = getTagProps({ index });
+    return (
+      <Chip
+        variant="soft"
+        color="neutral"
+        endDecorator={<ChipDelete key={key} variant="soft" {...tagProps} />}
+        sx={{ minWidth: 0 }}
+        key={key}
+      >
+        {item.text}
+      </Chip>
+    );
+  })
+);
