c0d8a62e99d0658b283d2d4164e823f161637060
Franco Reyes <franco.javier.reyes@gmail.com>
2024-07-27 14:52:26 +1000

    Tidy up spreadsheet

--- backend/src/reports/bookingGenerators.ts
+++ backend/src/reports/bookingGenerators.ts
@@ -1,20 +1,49 @@
 import { ReportGenerator } from "./index";
-import { booking } from "../../drizzle/schema";
-import { and, gte, inArray, lte } from "drizzle-orm";
+import { booking, person, space } from "../../drizzle/schema";
+import { and, desc, eq, gte, inArray, lte } from "drizzle-orm";
 import { db } from "../index";
 import XLSX from "xlsx";
+import { formatBookingDates } from "../utils";
 
 export const generateBookingSpreadsheet: ReportGenerator = async (startDate, endDate, spaces) => {
   const data = await db
     .select()
     .from(booking)
+    .innerJoin(person, eq(booking.zid, person.zid))
+    .innerJoin(space, eq(booking.spaceid, space.id))
     .where(and(
       gte(booking.starttime, startDate.toISOString()),
       lte(booking.endtime, endDate.toISOString()),
-      inArray(booking.spaceid, spaces),
-    ));
+      // inArray(booking.spaceid, spaces),
+    ))
+    .orderBy(desc(booking.endtime));
 
-  const worksheet = XLSX.utils.json_to_sheet(data);
+  let cleanedData: Record<string, string>[] = data
+    .map((row) => {
+      row.booking = formatBookingDates(row.booking);
+      return row;
+    })
+    .map(({ booking, person, space }) => ({
+      "Ref. No.": booking.id.toString(),
+      "Space": space.name,
+      "Date": new Date(booking.starttime).toLocaleDateString(),
+      "Start Time": new Date(booking.starttime).toLocaleTimeString(),
+      "End Time": new Date(booking.endtime).toLocaleTimeString(),
+      "User zID": "z" + person.zid,
+      "User Name": person.title + " " + person.fullname,
+      "User Role": person.role ?? "N/A",
+      "User Affiliation": person.school + "/" + person.faculty,
+    }));
+
+  const worksheet = XLSX.utils.json_to_sheet(cleanedData);
+
+  // Set column widths
+  if (cleanedData.length) {
+    worksheet["!cols"] = Object.keys(cleanedData[0]).map((key) => ({
+      wch: cleanedData.reduce((w, r) => Math.max(w, r[key].length), key.length) + 1
+    }));
+  }
+
   const workbook = XLSX.utils.book_new();
   XLSX.utils.book_append_sheet(workbook, worksheet, "Bookings");
 
