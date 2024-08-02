756db6b130081360ee5f724a4070f662e291d287
Tom Forsyth <tom4syth@gmail.com>
2024-08-02 18:02:35 +1000

    DeskIcon tests

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,27 +1,14 @@
-// module.exports = {
-//     preset: "ts-jest",
-//     testEnvironment: "jsdom",
-//     setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
-//     moduleNameMapper: {
-//       "^@/(.*)$": "<rootDir>/src/$1"
-//     },
-//     transform: {
-//         "^.+\\.tsx?$": "ts-jest"
-//     },
-// };
 const nextJest = require("next/jest");
 
-/** @type {import('jest').Config} */
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: "./",
 });
 
-// Add any custom config to be passed to Jest
 const config = {
   testEnvironment: "jsdom",
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/$1",
+    "^jose": require.resolve("jose"),
   },
   transformIgnorePatterns: [
     "/node_modules/"
@@ -29,5 +16,4 @@ const config = {
   setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
 };
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(config);
--- frontend/setupTests.ts
+++ frontend/setupTests.ts
@@ -1,1 +1,3 @@
 import "@testing-library/jest-dom";
+const { TextEncoder, TextDecoder } = require("util");
+Object.assign(global, { TextDecoder, TextEncoder });
